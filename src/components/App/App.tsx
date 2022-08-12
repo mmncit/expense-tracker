93289be0adecbbaa18b6fdb87e4b0987bfe332b1
mmncit <mamun.rashid@bentley.com>
2022-08-12 12:35:08 -0600

    expense item component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,9 @@
-import { Link } from "react-router-dom";
 import * as React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import { About } from "../About";
-import { fetchData } from "../../utils/network";
-
-function MainPage() {
-  const [profile, setProfile] = React.useState({});
-  React.useEffect(() => {
-    (async () => {
-      const profileInfo = await fetchData();
-      setProfile(profileInfo);
-    })();
-  }, []);
-  console.log({ profile });
-
-  return (
-    <div>
-      <Link to="/about">About</Link>
-      <h2>Github Pages</h2>
-      <h3>Deploying React to Github Pages</h3>
-    </div>
-  );
-}
-
+import { ExpenseItem } from "../ExpenseItem";
 export function App() {
   return (
     <React.StrictMode>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </HashRouter>
+      <ExpenseItem />
     </React.StrictMode>
   );
 }
---
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export const ExpenseItem = () => {
+  return <h2>Expense Item!</h2>;
+};
