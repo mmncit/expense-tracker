bf46d50298b5554889a7dea95c12a23cdd3d33b2
mmncit <mamun.rashid@bentley.com>
2022-08-13 13:03:45 -0600

    add css intoexpense item

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,4 +1,14 @@
 import * as React from "react";
+import "./ExpenseItem.css";
+
 export const ExpenseItem = () => {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 };
