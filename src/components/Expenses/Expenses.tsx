ceb437e2199c3ef88a4e6aa2788971297de54d04
mmncit <mamun.rashid@bentley.com>
2022-08-14 13:16:43 -0600

    add css to expenses

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { ExpenseItem } from "./ExpenseItem";
 import { Expense, ExpensesProps } from "./Expenses.types";
+import "./Expenses.css";
 
 export const Expenses = ({ expenses }: ExpensesProps) => {
   return (
-    <>
-      {expenses.map((expense: Expense) => (
-        <ExpenseItem
-          key={expense.id}
-          date={expense.date}
-          amount={expense.amount}
-          title={expense.title}
-        />
+    <div className="expenses">
+      {expenses.map(({ id, date, amount, title }: Expense) => (
+        <ExpenseItem key={id} date={date} amount={amount} title={title} />
       ))}
-    </>
+    </div>
   );
 };
